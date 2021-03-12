aa6789366fcdf5b370d38dca24385653870c8f13
giovannifreitas <giovannifr2015@gmail.com>
2021-03-11 22:32:50 -0300

    fixed for loop

--- code/python/alg_bubblesort.py
+++ code/python/alg_bubblesort.py
@@ -2,8 +2,8 @@
 def bubblesort(array):
     array_length = len(array)
     temp = 0
-    for i in range(array_length):
-        for j in range(array_length - 1):
+    for i in range(array_length - 1):
+        for j in range(array_length - i - 1):
             if array[j] > array[j + 1]:
                 temp = array[j + 1]
                 array[j + 1] = array[j]
